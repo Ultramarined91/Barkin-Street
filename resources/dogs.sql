295538eb5f86ac848be0efe99024795fc90b3027
Kai <limyikai91@gmail.com>
2018-11-07 17:34:43 +0800

    Pictures and database update

---
+++ resources/dogs.sql
@@ -0,0 +1,289 @@
+﻿USE f36im;
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "George",
+  "Poodle",
+  "Male",
+  7,
+  "Small",
+  "Shy and a little reserved.",
+  "A very gentle and sweet soul who is shy and a little reserved. Transforms into a ball of energy when you bring him out for a walk!",
+  0,
+  "07-george.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Brambles",
+  "Poodle",
+  "Female",
+  10,
+  "Small",
+  "Sweet-natured, friendly.",
+  "Brambles was found left to be consumed by the incoming tide under a bridge in Changi when he was 2 months old. He was sickly at that time but has proven against all odds that he is fighter and a survivor. He is always ready for a jog by the beach. He always comes out to greet whoever comes in, loves cuddling up to visitors who spend time getting to know him and always give kisses too.",
+  4,
+  "08-brambles.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Jumper",
+  "Poodle",
+  "Female",
+  5,
+  "Small",
+  "Sweet-natured, very friendly but shy.",
+  "Jumper is a friendly and affectionate girl. She was rescued from Pulau Ubin when she was 1.5 months old. She loves human contact and enjoys hugging her favourite humans by standing high on her hind legs and giving them bear hugs, licks and kisses. She loves to go for walks and also just sitting quietly bonding with her human friends and furry roommates.",
+  4,
+  "09-jumper.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "CX",
+  "Poodle",
+  "Female",
+  8,
+  "Small",
+  "Friendly, sweet-natured, laid-back.",
+  "CX is a very lovely, laid-back and friendly girl. CX stands for Chloe and the Greek warrior princess Xena. She gets along well with other dogs and doesn’t get angry or irritated even when other dogs bark at her. She loves going for walks and is also a very clever girl as she will follow your movement when you are walking her for example she will sit down beside you when you sit and will stand when you stand.",
+  0,
+  "10-cx.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Aho",
+  "Maltese",
+  "Male",
+  9,
+  "Small",
+  "Friendly, very laid back.",
+  "Aho used to be a guard dog at a construction site in Changi whom we befriended during our feeding route. We took him back to Barkin-Street when the construction work was completed and the workers vacated the site leaving Aho behind. He is very laid back and gentle with his mates and visitors alike.",
+  0,
+  "11-aho.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Igor",
+  "Maltese",
+  "Male",
+  7,
+  "Small",
+  "Very friendly and affectionate.",
+  "Igor was found hiding under precarious construction site debris. He is very loving and dotes on his favourite human friends. He is always welcomes people with a huge smile and sparkly eyes to greet people. He loves receiving kisses and also giving eager kisses to people. He gets along well with his roommate, Tigress.",
+  0,
+  "12-igor.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Tigress",
+  "Maltese",
+  "Female",
+  3,
+  "Small",
+  "Shy, reserved and friendly.",
+  "Tigress is one of 9 siblings who were being trapped by AVA at Pulau Ubin. She is a shy and reserved girl thus she was often overlooked by potential adopters. She is gentle natured and is good with mature kids and patient adults thus it is better for her to approach people first instead of people approaching her first. She gets along well with her roommate, Igor.",
+  0,
+  "13-tigress.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Ebal",
+  "Maltese",
+  "Female",
+  8,
+  "Small",
+  "Shy but friendly.",
+  "Ebal is a very shy but friendly girl. She has gained confidence walking on leash with her other kennel mates. She enjoys the company of dogs. Although still a little shy with people with the right and patient approach she warms up and feels safe.",
+  0,
+  "14-ebal.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Snowy",
+  "Maltese",
+  "Female",
+  6,
+  "Small",
+  "Sweet-natured, friendly, shy but will warm up with right/patient approach.",
+  "Snowy is very friendly and lovely towards people but she is also very shy. She loves treats so you get on her good side, become friends with her and bond with her by hand feeding her treats. Allow her to approach you first instead of you approaching her as it will boost her confidence and let her know that you are friendly towards her.",
+  0,
+  "15-snowy.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Chowder",
+  "Bichon Frise",
+  "Male",
+  6,
+  "Small",
+  "Playful and full of energy.",
+  "Chowder is very friendly and lovely towards everyone no matter the person is a stranger or not. He always shows his affection by welcoming people with kisses and asking for head rubs and pats. He is very obedient and highly trainable. Despite his age, he listens to commands well and is a fast learner. He also has bounds of energy and is always ready to head out for long walks!",
+  0,
+  "16-chowder.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Mack",
+  "Bichon Frise",
+  "Male",
+  5,
+  "Medium",
+  "Shy and quiet, but will warm up to affections.",
+  "Mack is one of Wave’s puppies. His siblings did not make it after birth, they passed away due to illness but Mack Mack had proved that he is a fighter and survivor. Like mother like son, he is very similar to his mother, Wave. He is very friendly and lovely towards everyone. He always shows his affection by welcoming people with kisses and asking for head rubs and pats. He's good in crowds, with kids, dogs and in the car - he appeared in a Porshe photo shoot! An overall chill gentle giant still looking for his home.",
+  0,
+  "17-mack.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Wave",
+  "Bichon Frise",
+  "Female",
+  9,
+  "Medium",
+  "Sweet-natured, very friendly, lovely.",
+  "Wave was pregnant when she was found at Changi and brought to the shelter where she gave birth to a litter of puppies. But sadly among her litter of puppies, only Mack Mack survived. She is very friendly and lovely towards everyone no matter who it is. She always shows her affection by welcoming people with kisses and asking for head rubs and pats. She will wait patiently for people to pat her and give her head rubs by standing beside them or sometimes putting her head under their hands. Like a mother will always do, she always wait for Mack Mack when volunteers bring them out for a walk. Both of them will wait for each other during their walk.",
+  0,
+  "18-wave.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Bam",
+  "Bichon Frise",
+  "Male",
+  7,
+  "Small",
+  "Friendly and very shy but will warm up with right/patient approach.",
+  "Bam is a very sweet boy. But he is timid and shy it comes to new faces/newcomers thus he usually hides behind his mother. He will warm up with the right and patient approach. When get to know him, he will accept/eat treats from you.",
+  0,
+  "19-bam.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Miracle",
+  "Bichon Frise",
+  "Female",
+  7,
+  "Small",
+  "Fearful and shy. Will open up slowly to handlers.",
+  "When she was found as a two-month-old puppy, she was trapped in a 15-foot-deep monsoon drain and the rescuers feared for her life as she could have easily been swept away by the currents. But even as a puppy, she was so strong-willed that she held firm and waited for help, attracting the rescuers’ attention with her cries. Some time later, she was adopted but unfortunately, we found out that she was cooped up in a small cage for hours. Her new family thought it was a normal to keep their pet confined to a little space where she could barely stand upright or move. Her rescuers once again carried her to safety back to the shelter.",
+  0,
+  "20-miracle.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Aziz",
+  "Corgi",
+  "Male",
+  4,
+  "Small",
+  "Full of energy and always greedy for food. A sweet boy who loves pats and lots of treats!",
+  "Aziz is one of the many strays that our founders feed every night in Tanah Merah. He has always been a lovable boy, often wagging his tail when food arrives. However in mid-January 2013, he was found with a vicious cut across his paw that left him limping in pain. Instead of rushing for his food like he always did, he laboriously and hesitantly crossed the busy street, with his paw bleeding. Trucks zoomed past, not paying any heed to him. As a result, our founder rescued him that very night and realised that he was running a high fever and had other open wounds on his body.",
+  0,
+  "21-aziz.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Sam",
+  "Corgi",
+  "Male",
+  4,
+  "Small",
+  "Patient, loving and very protective of the pretty heroine, his Josie.",
+  "There was a mass poisoning case by pest control in Tanah Merah Newater. Our founder and various AWGs scrambled to rescue the stray dogs that were residing there. They managed to rescue 13 dogs, out of which our founder took in Sam & Josie to OSCAS.",
+  0,
+  "22-sam.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Gypsy",
+  "Corgi",
+  "Female",
+  9,
+  "Medium",
+  "Sweet and affectionate girl.",
+  "The sweetest girl you’ll ever meet! A very affectionate girl who loves cuddles and walks. Great with children, would make a very good family pet.",
+  0,
+  "23-gypsy.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Xiao Hei",
+  "Corgi",
+  "Male",
+  9,
+  "Medium",
+  "A pretty and playful boy! Understands and learning commands like ‘sit’; also accepts treats politely and gently through the fence.",
+  "Xiao Bai was caged up from young since being newly acquired by his previous owners.  He never left the cage during his developmental stage.  Months later, the inevitable happened.  The helper of the house was attempting to clean the cage and was bitten. He was then deemd as vicious and aggressive and was sent away, with death as his fate looming. He was given a second chance in Barkin-Street.",
+  0,
+  "24-xiaohei.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Brownie",
+  "Husky",
+  "Female",
+  8,
+  "Large",
+  "Intelligent and playful.",
+  "Arriving to Barkin-Street as a pregnant mom she is the mother of many. She has a lot of affection to give. She's a gentle girl who has gained the confidence to walk on leash. She enjoys going out and smelling the outdoors and would be the sweetest addition to a loving home.",
+  0,
+  "25-brownie.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Destiny",
+  "Husky",
+  "Female",
+  5,
+  "Large",
+  "Shy and loves attention.",
+  "Destiny is a sweet lady who enjoys her walks and a good sniff around the neighbourhood. She is the ideal companion to chill out with.",
+  0,
+  "26-destiny.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Dexter",
+  "Golden Retriever",
+  "Male",
+  9,
+  "Large",
+  "Playful and warm. Responds well to his name",
+  "Dexter is still learning to trust people so needs a lot of patience and tender loving care. He loves treats which help him to come out of his shell and he has a playful & curious spirit too.",
+  0,
+  "27-dexter.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Tilly",
+  "Golden Retriever",
+  "Female",
+  7,
+  "Large",
+  "Affectionate and loves belly rubs.",
+  "Tilly loves to show her belly for rubs. She is a very sweet, affectionate and chilled out dog walking to her own pace, stopping of course for a rollover and belly rub, who would do well in a family who lives a similar kind of life!",
+  0,
+  "28-dexter.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Murano",
+  "Rottweiler",
+  "Female",
+  4,
+  "Medium",
+  "Sporty and energtic. Makes a great guard dog.",
+  "Murano gets on well with other dogs and loves human affection. He walks well on leash and always looks to check on how his human companion is going. He enjoys car rides too.",
+  0,
+  "29-murano.jpg"
+);
+
+INSERT INTO dogs (name, breed, gender, age, size, personality, description, adopter, image) VALUES (
+  "Captain",
+  "Rottweiler",
+  "Male",
+  8,
+  "Large",
+  "Shy infront of strangers. But active and playful around friends",
+  "Captain, now having gained confidence takes the lead to head out on walks. He has always shared his kennel with a female so enjoys canine company. He loves getting out for walks to explore the outdoors and would be best suited with an experienced dog handler who can make him feel comfortable, safe and let him explore.",
+  0,
+  "30-captain.jpg"
+);
